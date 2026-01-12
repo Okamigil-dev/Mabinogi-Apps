a5e2746d0f4167730e2bf8d3e73b7fa7fa73a6be
Okamigil-dev <Okamigil@hotmail.com>
2026-01-12 19:24:30 -0300

    Update nav.js
    testing

--- nav.js
+++ nav.js
@@ -138,7 +138,7 @@ class UserHeader extends HTMLElement {
         const defaultAvatar = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23a1a1aa'%3E%3Cpath d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z'/%3E%3C/svg%3E";
 
         if (user) {
-            statusEl.innerText = "Online";
+            statusEl.innerText = "Online test";
             statusEl.style.color = "var(--accent, #38bdf8)";
             
             // Try to get cached profile data (username/avatar) from localStorage
