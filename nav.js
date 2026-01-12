34d3d72199aa5f67ff4509e76c3b6374d6890a05
Okamigil-dev <Okamigil@hotmail.com>
2026-01-12 13:51:45 -0300

    Update nav.js
    testing idea

--- nav.js
+++ nav.js
@@ -30,6 +30,10 @@ class GlobalNav extends HTMLElement {
                 <svg class="nav-icon" viewBox="0 0 24 24"><path d="M9 18V5l12-2v13"/><circle cx="6" cy="18" r="3"/><circle cx="18" cy="16" r="3"/></svg>
                 SIMULATOR
             </a>
+            <a href="simulator.html" class="${this.isActive('simulator.html')}">
+                <svg class="nav-icon" viewBox="0 0 24 24"><path d="M9 18V5l12-2v13"/><circle cx="6" cy="18" r="3"/><circle cx="18" cy="16" r="3"/></svg>
+                TEST PAGE
+            </a>
         </nav>
         `;
     }
