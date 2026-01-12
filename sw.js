3fb756b55f99864e09c5f9fea1ed06d3581eb066
Okamigil-dev <Okamigil@hotmail.com>
2026-01-12 14:36:42 -0300

    Update sw.js
    changed global navigation bar to pull nav.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mabi-tools-v6-swr'; // You can keep this static mostly now
+const CACHE_NAME = 'mabi-tools-v6.1-swr'; // You can keep this static mostly now
 
 const ASSETS_TO_CACHE = [
   './',
